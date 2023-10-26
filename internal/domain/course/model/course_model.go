ff6d3ddd8bd4e56883f371e292e39e7ef5e668c1
mucha-fauzy <mucha.fauzy@gmail.com>
2023-10-27 04:02:46 +0700

    feat: add course model

--- internal/domain/course/model/course_model.go
+++ internal/domain/course/model/course_model.go
@@ -1,10 +1,26 @@
 package model
 
+import (
+	"time"
+
+	"github.com/guregu/null"
+)
+
 type Course struct {
-	Id         int    `db:"id"`
-	LecturerId string `db:"lecturer_id"`
-	Name       string `db:"name"`
-	Credit     int    `db:"credit"`
+	Id         string      `db:"id"`
+	LecturerId string      `db:"lecturer_id"`
+	Name       string      `db:"name"`
+	Credit     int         `db:"credit"`
+	CreatedAt  time.Time   `json:"createdAt"`
+	CreatedBy  string      `json:"createdBy"`
+	UpdatedAt  time.Time   `json:"updatedAt"`
+	UpdatedBy  string      `json:"updatedBy"`
+	DeletedAt  null.Time   `json:"deletedAt"`
+	DeletedBy  null.String `json:"deletedBy"`
 }
 
 type CourseList []*Course
+
+type CoursePrimaryID struct {
+	Id string `db:"id"`
+}
