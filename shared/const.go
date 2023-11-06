2d47b0ab21a30c83979adb3fa2f051fea1b766a2
mucha-fauzy <mucha.fauzy@gmail.com>
2023-11-06 17:37:41 +0700

    feat: add shared function

--- shared/const.go
+++ shared/const.go
@@ -3,4 +3,6 @@ package shared
 const (
 	DefaultPage     = 1
 	DefaultPageSize = 10
+	LecturerRole    = "lecturer"
+	AdminRole       = "admin"
 )
--- shared/utils.go
+++ shared/utils.go
@@ -1,9 +1,61 @@
 package shared
 
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/kks-learning-management-api/shared/failure"
+)
+
 func SliceStringToInterfaces(slices []string) []interface{} {
 	results := []interface{}{}
 	for _, s := range slices {
 		results = append(results, s)
 	}
 	return results
+}
+
+const (
+	IdKey    = "id"
+	RoleKey  = "role"
+	TokenKey = "token"
+)
+
+// GetIdFromContext retrieves the id from the request context.
+func GetIdFromContext(r *http.Request) (string, error) {
+	username, ok := r.Context().Value(IdKey).(string)
+	if !ok {
+		return "", failure.BadRequestFromString(fmt.Sprintf("`%s` not found in context", IdKey))
+	}
+	return username, nil
+}
+
+func GetRoleFromContext(r *http.Request) (string, error) {
+	role, ok := r.Context().Value(RoleKey).(string)
+	if !ok {
+		return "", failure.BadRequestFromString(fmt.Sprintf("`%s` not found in context", RoleKey))
+	}
+	return role, nil
+}
+
+func GetTokenFromContext(r *http.Request) (string, error) {
+	token, ok := r.Context().Value(TokenKey).(string)
+	if !ok {
+		return "", failure.BadRequestFromString(fmt.Sprintf("`%s` not found in context", TokenKey))
+	}
+	return token, nil
+}
+
+// WithId adds the id to the context.
+func WithId(ctx context.Context, username string) context.Context {
+	return context.WithValue(ctx, IdKey, username)
+}
+
+func WithRole(ctx context.Context, role string) context.Context {
+	return context.WithValue(ctx, RoleKey, role)
+}
+
+func WithToken(ctx context.Context, token string) context.Context {
+	return context.WithValue(ctx, TokenKey, token)
 }
